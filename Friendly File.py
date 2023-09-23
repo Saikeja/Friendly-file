9bbf8e6f88b030470413f4ebeece37d5e61ba5d6
Saikeja <104651380+Saikeja@users.noreply.github.com>
2023-09-23 13:05:39 -0400

    Add files via upload

---
+++ Friendly File.py
@@ -0,0 +1,128 @@
+name = input("Enter a name:")
+print("One day, there was a girl named " + name + ".")
+x = "animals that are easy to take care of."
+print("She loved " + x + "")
+print("She really wanted one to take care of since she LOVED animals.")
+p = "parents "
+print("But, her " + p + "hates pets very much that they disagreed on it.")
+age = input("Enter an age:")
+print("The girl's age was " + age + ".")
+a = "stand"
+subject = "girl"
+print("That's it! the " + subject + " could not " + a + " it!.")
+age = input("Enter the age you entered at first:")
+action = input("Enter an action:")
+print("Suddenly,a light bulb came in the " + age + " year old girl's head.")
+print("I can " + action + " an persuasive essay for my parents to read since the girl is very bad at essays.")
+action = input("Enter the word KNOWS:")
+print("The girl also " + action + " that she needs to work harder than usual to accomplish the difficult goal.")
+action = input("Enter a synonymy for please:")
+print("The girl " + action + " her parents for tutoring classes and her parents happily gave her classes about writing.")
+action = input("Enter the word DISTRACTED:")
+print("The girl tried to not get " + action + " in class and followed directions in class too.")
+print("One day in Topway Tutoring Center,she got a trophy that said 1ST PLACE on it. The girl kept talking about it.")
+noun = input("Enter the word: SKILLS:")
+print("The girl used her " + noun + " to write an persuasive essay about the positives for getting an goldfish.")
+feeling = "proud"
+print("She showed her work to her parents and her parents were so " + feeling + " of their daughter to be successful.")
+print("So, the parents decided to buy their daughter an goldfish for her birthday.")
+date = input("Enter an random birthday:")
+print("On " + date + " the girl were so surprised she got an goldfish for her birthday! She just keeps taking care of it and be generous to it.")
+print("The girl realized something: knowledge can also get what you want.")
+print("THE END")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
